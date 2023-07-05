62e83eb8bc7670cf6f2fd3cc87b4f84fd2069472
abineshkumar94 <abi1205.ak@gmail.com>
2023-07-05 09:35:00 +0530

    added characters

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let inputslider = document.getElementById("inputslider");
+    let slidervalue = document.getElementById("slidervalue");
+    let uppercase = document.getElementById("uppercase");
+    let lowercase = document.getElementById("lowercase");
+    let number = document.getElementById("number");
+    let symbol = document.getElementById("symbol");
+    let passBox = document.getElementById("passBox");
+    let genbtn = document.getElementById("genbtn");
+  
+    slidervalue.textContent = inputslider.value;
+    inputslider.addEventListener('input', () => {
+      slidervalue.textContent = inputslider.value;
+    });
+  
+    genbtn.addEventListener('click', () => {
+      passBox.value = generatePassword();
+    });
+  
+    function generatePassword() {
+      const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+      const numbers = '0123456789';
+      const symbols = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+      let password = '';
+      const length = inputslider.value;
+  
+      let selectedCharacters = '';
+      if (uppercase.checked) {
+        selectedCharacters += uppercaseLetters;
+      }
+      if (lowercase.checked) {
+        selectedCharacters += lowercaseLetters;
+      }
+      if (number.checked) {
+        selectedCharacters += numbers;
+      }
+      if (symbol.checked) {
+        selectedCharacters += symbols;
+      }
+  
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * selectedCharacters.length);
+        password += selectedCharacters[randomIndex];
+      }
+  
+      return password;
+    }
+  });
+  
